34945eff899ca9ef989569a23c24fd2851d9324a
marcosolito <bitlime.finance@gmail.com>
2022-09-02 16:53:33 +0200

    porto all'1% anche la commissione di getAmountIn nella library

--- contracts/libraries/UniswapV2Library.sol
+++ contracts/libraries/UniswapV2Library.sol
@@ -63,7 +63,7 @@ library UniswapV2Library {
         require(amountOut > 0, 'UniswapV2Library: INSUFFICIENT_OUTPUT_AMOUNT');
         require(reserveIn > 0 && reserveOut > 0, 'UniswapV2Library: INSUFFICIENT_LIQUIDITY');
         uint numerator = reserveIn.mul(amountOut).mul(1000);
-        uint denominator = reserveOut.sub(amountOut).mul(997);
+        uint denominator = reserveOut.sub(amountOut).mul(990);
         amountIn = (numerator / denominator).add(1);
     }
 
